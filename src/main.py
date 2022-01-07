f29cd45cb1db261141197a03a47c77e302b52b59
Fedor Blinov <fedyoiblinov@gmail.com>
2022-01-07 20:00:36 +0300

    Basic application class and run setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,45 @@
-print("Hello World!")
+import pygame
+from core.field import Grid
+from pathlib import Path
+
+
+class Main:
+    def __init__(self):
+        pygame.init()
+
+        self.size = self.width, self.height = 800, 800
+        self.screen = pygame.display.set_mode(self.size)
+
+        self.HOME_DIR = Path(__file__).resolve().parent.parent
+        self.CONFIG_DIR = Path.joinpath(self.HOME_DIR, 'configs')
+
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.pause = True
+
+        self.FPS = 60
+
+        # ключ уровня и сложность потом будем получать из другого класса-меню
+
+        self.lvl = 'forest'
+
+        self.difficulty = 'easy'
+
+        self.grid = Grid(self.size, (self.HOME_DIR, self.CONFIG_DIR), self.difficulty, self.lvl)
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            self.grid.render(self.screen)
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
+
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    app = Main()
+    app.run()
