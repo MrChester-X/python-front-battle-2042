0d7ea7a02db15b4625502a16326a90938013f59a
MrChester-X <bulychev_platon1@mail.ru>
2022-01-10 22:39:55 +0300

    Unit controller and some small fixes

---
+++ src/globals/main_globals.py
@@ -0,0 +1,7 @@
+from config.json_controller import FileJSON
+
+json_units = FileJSON("../configs/units.json")
+json_enemies = FileJSON("../configs/enemies.json")
+json_maps = FileJSON("../configs/maps.json")
+
+units = {}
--- src/main.py
+++ src/main.py
@@ -1,1 +1,3 @@
-print("Hello World!")
+from units.unit_controller import load_all_units
+
+load_all_units()
