580e3d03191cbc14e6258e63748ec2833788bc19
Qarkarich <48867128+Qarkarich@users.noreply.github.com>
2022-01-17 21:56:43 +0300

    Update 1.1 (#7)
    * Added __init__.py to src package
    
    * Enemies
    
    * Image loader and controller class
    
    * Sprite support for enemies

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import pygame
 from core.field import Grid
 from pathlib import Path
+from src.enemies import enemies_lib
+from src.config.json_controller import FileJSON
+import sys
 
 
 class Main:
     def __init__(self):
         pygame.init()
 
-        self.size = self.width, self.height = 800, 800
+        self.size = self.width, self.height = 1000, 1000
         self.screen = pygame.display.set_mode(self.size)
 
         self.HOME_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,20 @@ class Main:
 
         self.difficulty = 'easy'
 
-        self.grid = Grid(self.size, (self.HOME_DIR, self.CONFIG_DIR), self.difficulty, self.lvl)
+        try:
+            self.json_controller = FileJSON(Path.joinpath(self.CONFIG_DIR, 'maps.json'))
+            self.settings = \
+                list(filter(lambda x: x['key'] == self.lvl,
+                            self.json_controller.get_json()['maps']))[0]
+        except FileNotFoundError:
+            print('Файла конфигурации не существует')
+            sys.exit()
+
+        self.grid = Grid(self.size, (self.HOME_DIR, self.CONFIG_DIR), self.settings)
+
+        zombie1 = enemies_lib.Zombie()
+
+        self.grid.add(zombie1)
 
     def run(self):
         while self.running:
@@ -34,7 +50,12 @@ class Main:
                     self.running = False
 
             self.grid.render(self.screen)
+
+            self.grid.draw(self.screen)
+            self.grid.update()
+
             pygame.display.flip()
+
             self.clock.tick(self.FPS)
 
         pygame.quit()
